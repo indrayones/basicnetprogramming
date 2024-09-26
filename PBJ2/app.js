7f6d4f700d4b3bdc4730190840f02123e33aed37
indrayones <indrayones912@gmail.com>
2024-09-27 00:29:47 +0700

    Update PBJ2

--- PBJ2/app.js
+++ PBJ2/app.js
@@ -1,7 +1,87 @@
-// Menggunakan dynamic import untuk chalk
-(async () => {
-    const chalk = (await import('chalk')).default;
-  
-    // Mencetak teks dengan chalk
-    console.log(chalk.blue('print warna biru sukses'));
-  })();
+const yargs = require('yargs');
+const catatan = require('./catatan.js')
+
+// Kustomisasi versi yargs
+yargs.version('10.1.0')
+
+// Membuat perintah (command) 'tambah'
+yargs.command({
+    command: 'tambah',
+    describe: 'tambah sebuah catatan baru',
+    builder: {
+        judul: {
+            describe: 'Judul catatan',
+            demandOption: true,
+            type: 'string'
+        },
+        isi: {
+            describe: 'Isi catatan',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        catatan.tambahCatatan(argv.judul, argv.isi)
+    }
+})
+
+yargs.command({
+    command: 'hapus',
+    describe: 'Hapus catatan',
+    builder: {
+        judul: {
+            describe: 'Judul catatan yang ingin dihapus',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        catatan.hapusCatatan(argv.judul)
+    }
+})
+
+// Perintah untuk menampilkan semua catatan
+yargs.command({
+    command: 'list',
+    describe: 'Tampilkan semua catatan',
+    handler: function () {
+        console.log('Menampilkan semua catatan')
+    }
+})
+// Perintah untuk menampilkan semua catatan
+yargs.command({
+    command: 'list',
+    describe: 'Tampilkan semua catatan',
+    handler: function () {
+        catatan.listCatatan()
+    }
+})
+
+// Perintah untuk membaca catatan
+yargs.command({
+    command: 'baca',
+    describe: 'Baca sebuah catatan',
+    builder: {
+        judul: {
+            describe: 'Judul catatan yang ingin dibaca',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        catatan.bacaCatatan(argv.judul)
+    }
+})
+
+
+// Perintah untuk membaca catatan
+yargs.command({
+    command: 'baca',
+    describe: 'Baca sebuah catatan',
+    handler: function () {
+        console.log('Membaca sebuah catatan')
+    }
+})
+
+// Parsing yargs
+yargs.parse()
--- PBJ2/catatan.js
+++ PBJ2/catatan.js
@@ -1,4 +1,76 @@
-const ambilCatatan = function () {
-    return 'Ini Catatan Indra..'
+const fs = require('fs')
+const chalk = require('chalk')
+
+const muatCatatan = function () {
+    try {
+        const dataBuffer = fs.readFileSync('catatan.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
     }
-module.exports = ambilCatatan
+}
+
+const listCatatan = function () {
+    const catatan = muatCatatan()
+    
+    console.log(chalk.inverse('Daftar Semua Catatan:'))
+
+    catatan.forEach((note) => {
+        console.log(note.judul)
+    })
+}
+
+const bacaCatatan = function (judul) {
+    const catatan = muatCatatan()
+    const catatanDicari = catatan.find((note) => note.judul === judul)
+    
+    if (catatanDicari) {
+        console.log(chalk.inverse(catatanDicari.judul))
+        console.log(catatanDicari.isi)
+    } else {
+        console.log(chalk.red.inverse('Catatan tidak ditemukan!'))
+    }
+}
+
+const simpanCatatan = function (catatan) {
+    const dataJSON = JSON.stringify(catatan)
+    fs.writeFileSync('catatan.json', dataJSON)
+}
+
+const tambahCatatan = function (judul, isi) {
+    const catatan = muatCatatan()
+    const duplikatCatatan = catatan.find((note) => note.judul === judul)
+
+    if (!duplikatCatatan) {
+        catatan.push({
+            judul: judul,
+            isi: isi
+        })
+        simpanCatatan(catatan)
+        console.log(chalk.green.inverse('Catatan baru ditambahkan!'))
+    } else {
+        console.log(chalk.red.inverse('Judul catatan sudah ada!'))
+    }
+}
+
+const hapusCatatan = function (judul) {
+    const catatan = muatCatatan()
+    const catatanUntukDisimpan = catatan.filter(function (note) {
+        return note.judul !== judul
+    })
+    
+    if (catatan.length > catatanUntukDisimpan.length) {
+        console.log(chalk.green.inverse('Catatan dihapus!'))
+        simpanCatatan(catatanUntukDisimpan)
+    } else {
+        console.log(chalk.red.inverse('Catatan tidak ditemukan!'))
+    }
+}
+
+module.exports = {
+    tambahCatatan: tambahCatatan,
+    hapusCatatan: hapusCatatan,
+    listCatatan: listCatatan, 
+    bacaCatatan: bacaCatatan   
+};
